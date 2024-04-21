dfdab7f992b6e9436a06793bdf69cfd2c4d5f30f
catid <mrcatid@gmail.com>
2024-04-21 20:35:07 +0000

    fix

--- catid_distributed_eval_8.sh
+++ catid_distributed_eval_8.sh
@@ -4,7 +4,7 @@ cd lm-evaluation-harness
 conda create -n lmeval python=3.10 -y && conda activate lmeval
 pip install -e .
 
-accelerate launch --config_file accelerate_config.yaml -m lm_eval --model hf \
+accelerate launch --config_file ../accelerate_config.yaml -m lm_eval --model hf \
     --model_args pretrained=catid/cat-llama-3-8b-instruct-aqlm \
     --tasks winogrande,piqa,hellaswag,arc_easy,arc_challenge \
     --batch_size 16
